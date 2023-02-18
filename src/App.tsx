d272304927bb3891d85090d56fa58a5ee5afce83
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-02-18 17:47:38 +0300

    samurai road

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
+    <div>
+    <div>one div</div>
     <div className="App">
-     Hello, samurai! Let's go!
+     <ul>
+      <li>js</li>
+      <li>react</li>
+      <li>html</li>
+      <li>css</li>
+     </ul>
+    </div>
     </div>
   );
 }
