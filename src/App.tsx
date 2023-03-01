7a72b7b76591a5963bc31c5b13b99752c893ab85
Matthewdeveloper1 <92750111+Matthewdeveloper1@users.noreply.github.com>
2023-03-01 18:17:01 +0300

    SocialStart

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
+
 
 const App = () => {
   return (
-    <div>
-    <div>one div</div>
-    <div className="App">
-     <ul>
-      <li>js</li>
-      <li>react</li>
-      <li>html</li>
-      <li>css</li>
-     </ul>
-    </div>
+    <div className='app-wrapper'>
+      <Header/>
+      <Navbar/>
+      <Profile/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+ import React from "react"; 
+
+
+ const Header = () => {
+  return(
+    <header className='header'>
+      <img src='https://pngicon.ru/file/uploads/naruto.png' />
+    </header>
+  )
+ }
+
+ export default Header;
